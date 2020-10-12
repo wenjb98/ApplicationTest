9b209f44c9b25c1b84d00fa55636a96a1c18aa7c
wen <599483010@qq.com>
2020-10-12 13:20:35 +0800

    second commit

--- app/src/main/java/com/example/applicationtest/MainActivity.java
+++ app/src/main/java/com/example/applicationtest/MainActivity.java
@@ -10,11 +10,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button1 ;
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+    private  double dmi;
+    private Button cbutton ;
+    private EditText bmiheight;
+    private EditText bmiweight;
+    public DecimalFormat df = new DecimalFormat("#.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,17 +68,23 @@ public class MainActivity extends AppCompatActivity {
      * 初始化视图
      */
     public void initView(){
-        button1 =  findViewById(R.id.button1);
+        cbutton =  findViewById(R.id.CButton);
+        bmiheight = findViewById(R.id.BMIHeght);
+        bmiweight = findViewById(R.id.BMIWeight);
 
         //为按钮对象添加监听器
-        button1.setOnClickListener(new View.OnClickListener() {
+        cbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(MainActivity.this, "hello activity", Toast.LENGTH_LONG).show();
 
                 //显示添加意图Intent
 
+                //获取输入框中的内容
+                double height = Double.parseDouble(bmiheight.getText().toString());
+                double weight = Double.parseDouble(bmiweight.getText().toString());
 
+                System.out.println(df.format( weight/(height*height)));
             }
         });
     }
